08c660bcdcb10cbe1fd7dad6508f3406e3bcb546
Baileigh <bai1eigh@outlook.com>
2022-07-23 21:25:41 -0400

    took away routes. Unsure of issues will program

--- server.js
+++ server.js
@@ -1,25 +1,85 @@
-//setting up dependencies ('outside code that the application depends on')
-//nodejs and its framworks || routes
-const express = require('express');
-const path = require('path');
-const api = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes');
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 
-//setting up server
+
+// Handling Asynchronous Processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+// Setting Up Server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
-//middleware lets static files process
-app.use(express.static('./develop/public'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-app.use('/', htmlRoutes);
+app.use(express.json());
 
-//console.log to see if the server is running locally
-console.log('program started')
 
-//listening to server
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// Static Middleware
+app.use(express.static("./develop/public"));
+
+
+// API Route | "GET" request
+app.get("/api/notes", function(req, res) {
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+      notes = [].concat(JSON.parse(data))
+      res.json(notes);
+    })
+}); 
+
+
+// API Route | "POST" request
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1
+      notes.push(note);
+      return notes
+    }).then(function(notes) {
+      writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+      res.json(note);
+    })
 });
+
+
+// API Route | "DELETE" request
+app.delete("/api/notes/:id", function(req, res) {
+  const idToDelete = parseInt(req.params.id);
+  readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotesData = []
+    for (let i = 0; i<notes.length; i++) {
+      if(idToDelete !== notes[i].id) {
+        newNotesData.push(notes[i])
+      }
+    }
+    return newNotesData
+  }).then(function(notes) {
+    writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+    res.send('saved success!!!');
+  })
+})
+
+
+// HTML Routes
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+  });
+
+app.get("/", function(req, res) {
+     res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+ });
+
+
+// Listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
